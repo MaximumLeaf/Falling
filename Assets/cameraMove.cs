d0c80aba9b9f1b6aadcaf8624408f9d56c2921c0
MaximumLeaf <32802600+MaximumLeaf@users.noreply.github.com>
2017-10-21 14:52:27 -0400

    ghffd

---
+++ Assets/cameraMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour {
+
+	private Rigidbody2D rb2d;			//make it so the camera follows player on y axis, but not x axis
+	public Rigidbody2D player;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb2d.position = new Vector2 (0, player.position.y);
+	}
+}
